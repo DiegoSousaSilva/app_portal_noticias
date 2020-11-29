43f25d7139ead5cf9ebd10778fe3b6299693725c
Diego Sousa <derickbenji2@gmail.com>
2020-11-29 14:23:11 -0300

    atualisando pagina  Home adcionando img local

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,22 +2,26 @@ import React from 'react';
 import { Button, ImageBackground, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 
-const image ={ uri: 'https://reactjs.org/logo-og.png'};
+//const image ={ uri: 'https://reactjs.org/logo-og.png'};
+const image = require('../assets/onepiecenoticia.jpg');
 
 export default function Home({navigation}) {
   return (
     <View style={{flex:1}}>
-      <ScrollView horizontal contentContainerStyle={{width:'200%', height: 250}} style={{flex:1}}>
+      <ScrollView horizontal contentContainerStyle={{width:'200%', height: 200}} style={{flex:1}}>
         <ImageBackground source={image} style={styles.image}>
-          <View style={{position:'absolute', left:0, top:0, width:'100%', height:'100%', backgroundColor: 'rgba(0,0,0,0.5)'}}></View>
-          <Text style={styles.txtImage}>Noticia principal</Text>
+          <TouchableOpacity onPress={()=> navigation.navigate('Noticias')} style={{width:'100%', height:'100%', backgroundColor: 'rgba(0,0,0,0.5)', justifyContent:'flex-end'}}>
+            <Text style={styles.txtImage}>One Piece está disponível na Netflix</Text>
+          </TouchableOpacity>
+        </ImageBackground>
+
+        <ImageBackground source={image} style={styles.image}>
+          <TouchableOpacity style={{width:'100%', height:'100%', backgroundColor: 'rgba(0,0,0,0.5)', justifyContent:'flex-end'}}>
+            <Text style={styles.txtImage}>One Piece está disponível na Netflix</Text>
+          </TouchableOpacity>
         </ImageBackground>
       </ScrollView>
       
-
-      <TouchableOpacity onPress={()=> navigation.navigate('Noticias')}>
-        <Text>Noticias</Text>
-      </TouchableOpacity>
       <StatusBar hidden/>
     </View>
   )
@@ -28,10 +32,10 @@ const styles = StyleSheet.create({
     flex:1,
     resizeMode:'cover',
     justifyContent: 'flex-end',
-    width: '50%'
+    width: '100%'
   },
   txtImage:{
     color: '#fff',
-    fontSize: 30
+    fontSize: 22
   }
 })
