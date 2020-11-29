dc67772ec752544012f50cf4cf330dc48b8fae49
Diego Sousa <derickbenji2@gmail.com>
2020-11-29 16:32:40 -0300

    recebendo dados via parametro

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,22 +2,35 @@ import React from 'react';
 import { Button, ImageBackground, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 
-//const image ={ uri: 'https://reactjs.org/logo-og.png'};
+const image2 ={ uri: 'https://uploads.jovemnerd.com.br/wp-content/uploads/2020/02/one-piece-manga-adiado.jpg'};
 const image = require('../assets/onepiecenoticia.jpg');
+
 
 export default function Home({navigation}) {
   return (
     <View style={{flex:1}}>
       <ScrollView horizontal contentContainerStyle={{width:'200%', height: 200}} style={{flex:1}}>
-        <ImageBackground source={image} style={styles.image}>
-          <TouchableOpacity onPress={()=> navigation.navigate('Noticias')} style={{width:'100%', height:'100%', backgroundColor: 'rgba(0,0,0,0.5)', justifyContent:'flex-end'}}>
+        <ImageBackground source={image} style={styles.imageStyle}>
+          <TouchableOpacity 
+            onPress={()=> navigation.navigate('Noticias',{
+              titulo: 'One Piece está disponível na Netflix',
+              conteudo: 'One Piece chegou ao catálogo da Netflix hoje (12), e agora é possível acompanhar a aventura dos Piratas do Chapéu de Palha com dublagem em português na plataforma.',
+              img: image,
+            })} 
+            style={styles.touchableStyle}>
             <Text style={styles.txtImage}>One Piece está disponível na Netflix</Text>
           </TouchableOpacity>
         </ImageBackground>
 
-        <ImageBackground source={image} style={styles.image}>
-          <TouchableOpacity style={{width:'100%', height:'100%', backgroundColor: 'rgba(0,0,0,0.5)', justifyContent:'flex-end'}}>
-            <Text style={styles.txtImage}>One Piece está disponível na Netflix</Text>
+        <ImageBackground source={image2} style={styles.imageStyle}>
+          <TouchableOpacity 
+            onPress={()=> navigation.navigate('Noticias',{
+              titulo: 'Novo capítulo do mangá é adiado temporariamente',
+              conteudo: 'Apesar das estimativas de Eiichiro Oda, criador de One Piece, de que o mangá deve ser concluído nos próximos anos (leia mais aqui) isso ainda não está exatamente perto de acontecer. A franquia continua super popular e os capítulos continuam sendo publicados semanalmente.',
+              img: image2,
+            })}
+            style={styles.touchableStyle}>
+            <Text style={styles.txtImage}>Novo capítulo do mangá é adiado temporariamente</Text>
           </TouchableOpacity>
         </ImageBackground>
       </ScrollView>
@@ -28,7 +41,7 @@ export default function Home({navigation}) {
 }
 
 const styles = StyleSheet.create({
-  image:{
+  imageStyle:{
     flex:1,
     resizeMode:'cover',
     justifyContent: 'flex-end',
@@ -36,6 +49,12 @@ const styles = StyleSheet.create({
   },
   txtImage:{
     color: '#fff',
-    fontSize: 22
+    fontSize: 18
+  },
+  touchableStyle:{
+    width:'100%', 
+    height:'100%', 
+    backgroundColor: 'rgba(0,0,0,0.5)', 
+    justifyContent:'flex-end'
   }
 })
--- src/pages/Notcias.js
+++ src/pages/Notcias.js
@@ -1,18 +1,51 @@
 import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, ScrollView, TouchableOpacity, View, ImageBackground} from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 
-export default function Noticias({navigation}) {
+
+export default function Noticias({route, navigation}) {
+  const image = route.params.img;
   return (
-    <View>
-      <StatusBar hidden/>
-      <Text>Noticias</Text>
-      <Button 
-        title="Ir para Home"
+    <View style={styles.container}>
+      <ScrollView contentContainerStyle={{width:'100%', height: 250}} >
+        <StatusBar hidden/>
+        <ImageBackground style={styles.imageStyle} source={image} ></ImageBackground>
+        <Text style={styles.titleText}>{route.params.titulo}</Text>
+        <Text style={styles.conteudoText}>{route.params.conteudo}</Text>
+
+      </ScrollView>
+      <TouchableOpacity 
+        style={{position: 'absolute', bottom: 0}}
         onPress={()=> navigation.navigate('Portal')}
-      />
-    </View>
+      >
+        <Text>Voltar</Text>
+      </TouchableOpacity>
+    </View>  
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding:8,
+    marginLeft: 8,
+    marginRight: 8,
+    backgroundColor: '#ccc'
+  },
+  titleText:{
+    fontSize: 14,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 10
+  },
+  conteudoText:{
+    fontSize: 13
+  },
+  imageStyle:{
+    flex:1,
+    resizeMode:'cover',
+    justifyContent: 'flex-end',
+    width: '100%',
+    height: '100%'
+  },
+})
